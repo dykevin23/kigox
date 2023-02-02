f82f087ca520e2a38b42e7bd8984a5e659f50574
Daryl <dyk0203@naver.com>
2023-02-02 14:50:56 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/kigox" : "",
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
