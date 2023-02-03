6b5d006dd1ba52361fddd83fd8d828de57586996
Daryl <dyk0203@naver.com>
2023-02-03 15:34:35 +0900

    next config & github pages deploy test

---
+++ config/config.ts
@@ -0,0 +1,4 @@
+export const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://dykevin23.github.io/kigox"
+    : "";
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/",
+  basePath: "/kigox",
   assetPrefix: process.env.NODE_ENV === "production" ? "/kigox" : "",
   trailingSlash: true,
 };
