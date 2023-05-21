48b6005db6d54b9c1e5a116a82f36fa6e5f185cf
Daryl <dyk0203@naver.com>
2023-05-22 01:53:06 +0900

    install firebase and setting firebase config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import firebase from "../firebase";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // console.log("firebase => ", firebase);
   return (
     <div className="w-full max-w-lg mx-auto">
       <Component {...pageProps} />
