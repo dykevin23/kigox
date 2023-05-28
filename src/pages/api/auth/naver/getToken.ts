33631cfd332e4a3517399ec5a73c9060db25a5cb
Daryl <dyk0203@naver.com>
2023-05-29 00:52:14 +0900

    test

--- src/pages/api/auth/naver/getToken.ts
+++ src/pages/api/auth/naver/getToken.ts
@@ -10,15 +10,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { url, method, body } = req;
+  const { method, body } = req;
 
-  if (url?.includes("naver")) {
-    const result = await axios({
-      url: "https://nid.naver.com/oauth2.0/token",
-      method,
-      data: body,
-    });
+  console.log("### success call api");
 
-    res.status(200).json({ data: result });
-  }
+  const result = await axios({
+    url: "https://nid.naver.com/oauth2.0/token",
+    method,
+    data: body,
+  });
+
+  res.status(200).json({ data: result });
 }
