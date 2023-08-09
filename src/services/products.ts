c578f88a552d13ed3ca0db7a1b03617340602f00
kevin <dyk0203@naver.com>
2023-08-09 10:19:00 +0900

    카테고리 api

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,5 +1,7 @@
 import { callApi } from "@common/utils/client/axiosInstances";
+import { ResponseType } from "@common/utils/server/withHandler";
 import { ProductsRequestParams } from "@pages/api/products";
+import { IMainCategory } from "types/metadataType";
 
 export const products = async ({ childId }: ProductsRequestParams) => {
   return await callApi({
@@ -7,4 +9,13 @@ export const products = async ({ childId }: ProductsRequestParams) => {
     method: "GET",
     params: { childId },
   });
+};
+
+export const category = async () => {
+  const result = await callApi<ResponseType<IMainCategory[]>>({
+    url: "/api/products/category",
+    method: "GET",
+  });
+
+  return result.result
 };
