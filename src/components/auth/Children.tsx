d4d3c881d2cb46d9ddf0ca40fff07c69bdf018a1
kevin <dyk0203@naver.com>
2023-09-04 18:08:01 +0900

    회원가입 자녀정보 디자인  개발중

--- src/components/auth/Children.tsx
+++ src/components/auth/Children.tsx
@@ -10,18 +10,38 @@ interface ChildrenProps {
 
 const Children = (props: ChildrenProps) => {
   const {
-    childrens: { fields, append },
+    childrens: { fields, append, remove },
   } = props;
 
   const handleAddChild = () => {
     append({ nickname: "", birthday: "", gender: "male" });
   };
 
+  const handleDeleteChild =
+    (index: number) => (event: React.MouseEvent<HTMLButtonElement>) => {
+      remove(index);
+    };
+
   return (
-    <div className="flex flex-col gap-3 mt-3">
-      <span>자녀정보</span>
+    <div className="flex flex-col gap-3">
+      <div className="flex gap-2 items-center">
+        <div className="w-1 h-6 bg-amber-800" />
+        <span className="text-lg font-medium text-amber-800">자녀정보</span>
+      </div>
+      <div>
+        <span className="text-sm">
+          자녀를 등록해주세요. 자녀를 미등록 시 추천 서비스 이용에 제한될 수
+          있습니다.
+        </span>
+      </div>
       {fields.map((_, index: number) => {
-        return <Child key={index} childIndex={index} />;
+        return (
+          <Child
+            key={index}
+            childIndex={index}
+            onDelete={handleDeleteChild(index)}
+          />
+        );
       })}
       <Button label="자녀추가" onClick={handleAddChild} />
     </div>
@@ -30,17 +50,24 @@ const Children = (props: ChildrenProps) => {
 
 interface ChildProps {
   childIndex: number;
+  onDelete: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 const Child = (props: ChildProps) => {
-  const { childIndex } = props;
+  const { childIndex, onDelete } = props;
 
   const {
     formState: { errors },
   } = useFormContext();
 
   return (
-    <div className="flex flex-col gap-1">
-      <span>{`자녀${childIndex + 1}`}</span>
+    <div className="flex flex-col gap-1 ">
+      <div className="flex justify-between mx-2 items-center">
+        <span>{`자녀${childIndex + 1}`}</span>
+        <div className="w-15">
+          <Button label="삭제" onClick={onDelete} />
+        </div>
+      </div>
+
       <Input
         name={`children.${childIndex}.nickname`}
         placeholder="닉네임을 입력해주세요.(10자 이내)"
--- src/components/common/elements/form/Form.tsx
+++ src/components/common/elements/form/Form.tsx
@@ -10,7 +10,7 @@ const Form = (props: FormProps) => {
   const { label = "", onSubmit, children } = props;
   return (
     <>
-      <form className="flex flex-col pt-3 pb-20 m-3 gap-2">{children}</form>
+      <form className="flex flex-col pt-3 pb-20 mx-3 gap-2">{children}</form>
       <div
         className="fixed bottom-0 w-full flex items-center justify-center h-14 bg-yellow-300"
         onClick={onSubmit}
